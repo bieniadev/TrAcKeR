64beff50ef02dfb33ed85daf7beec4e50c20ecfa
BuildTools <unconfigured@null.spigotmc.org>
2024-06-05 17:14:01 +0200

    keyboard track fix

--- lib/components/add_expence_modal.dart
+++ lib/components/add_expence_modal.dart
@@ -109,133 +109,137 @@ class _AddExpenceModalState extends ConsumerState<AddExpenceModal> {
     List<Expence> expencesList = ref.read(expencesListProvider).map((dynamicItem) => dynamicItem as Expence).toList();
     List<String> categoryList = expencesList.map((expence) => expence.category).toSet().toList();
     return Padding(
-      padding: const EdgeInsets.all(14),
-      child: Column(
-        children: [
-          const SizedBox(height: 6),
-          Text('Add Expence', style: GoogleFonts.josefinSans(color: Theme.of(context).colorScheme.onSecondary, fontSize: 22)),
-          const SizedBox(height: 12),
-          Row(
-            children: [
-              Flexible(
-                flex: 5,
-                child: TextField(
-                  controller: descController,
-                  keyboardType: TextInputType.text,
-                  style: GoogleFonts.josefinSans(color: Theme.of(context).colorScheme.inversePrimary, fontSize: 16),
-                  cursorColor: Theme.of(context).colorScheme.onPrimary,
-                  decoration: InputDecoration(
-                    label: Text('Description', style: GoogleFonts.josefinSans(color: Theme.of(context).colorScheme.onPrimary, fontSize: 16)),
-                    filled: true,
-                    fillColor: Theme.of(context).colorScheme.primary,
-                    enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).colorScheme.inversePrimary.withOpacity(0.65)), borderRadius: BorderRadius.circular(10)),
-                    focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).colorScheme.onPrimary), borderRadius: BorderRadius.circular(10)),
+      padding: MediaQuery.of(context).viewInsets,
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const SizedBox(height: 6),
+            Text('Add Expence', style: GoogleFonts.josefinSans(color: Theme.of(context).colorScheme.onSecondary, fontSize: 22)),
+            const SizedBox(height: 12),
+            Row(
+              children: [
+                Flexible(
+                  flex: 5,
+                  child: TextField(
+                    controller: descController,
+                    keyboardType: TextInputType.text,
+                    style: GoogleFonts.josefinSans(color: Theme.of(context).colorScheme.inversePrimary, fontSize: 16),
+                    cursorColor: Theme.of(context).colorScheme.onPrimary,
+                    decoration: InputDecoration(
+                      label: Text('Description', style: GoogleFonts.josefinSans(color: Theme.of(context).colorScheme.onPrimary, fontSize: 16)),
+                      filled: true,
+                      fillColor: Theme.of(context).colorScheme.primary,
+                      enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).colorScheme.inversePrimary.withOpacity(0.65)), borderRadius: BorderRadius.circular(10)),
+                      focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).colorScheme.onPrimary), borderRadius: BorderRadius.circular(10)),
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(width: 10),
-              Flexible(
-                flex: 2,
-                child: TextField(
-                  controller: amountController,
-                  keyboardType: TextInputType.number,
-                  style: GoogleFonts.josefinSans(color: Theme.of(context).colorScheme.inversePrimary, fontSize: 16),
-                  cursorColor: Theme.of(context).colorScheme.onPrimary,
-                  decoration: InputDecoration(
-                    label: Text('Amount', style: GoogleFonts.josefinSans(color: Theme.of(context).colorScheme.onPrimary, fontSize: 16)),
-                    filled: true,
-                    fillColor: Theme.of(context).colorScheme.primary,
-                    enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).colorScheme.inversePrimary.withOpacity(0.65)), borderRadius: BorderRadius.circular(10)),
-                    focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).colorScheme.onPrimary), borderRadius: BorderRadius.circular(10)),
+                const SizedBox(width: 10),
+                Flexible(
+                  flex: 2,
+                  child: TextField(
+                    controller: amountController,
+                    keyboardType: TextInputType.number,
+                    style: GoogleFonts.josefinSans(color: Theme.of(context).colorScheme.inversePrimary, fontSize: 16),
+                    cursorColor: Theme.of(context).colorScheme.onPrimary,
+                    decoration: InputDecoration(
+                      label: Text('Amount', style: GoogleFonts.josefinSans(color: Theme.of(context).colorScheme.onPrimary, fontSize: 16)),
+                      filled: true,
+                      fillColor: Theme.of(context).colorScheme.primary,
+                      enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).colorScheme.inversePrimary.withOpacity(0.65)), borderRadius: BorderRadius.circular(10)),
+                      focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).colorScheme.onPrimary), borderRadius: BorderRadius.circular(10)),
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 10),
-          Row(
-            children: [
-              Flexible(
-                flex: 1,
-                child: DropDownTextField(
-                  dropDownList: List.generate(categoryList.length, (index) => DropDownValueModel(name: categoryList[index], value: index)),
-                  controller: categoryDropdownController,
-                  enableSearch: true,
-                  keyboardType: TextInputType.text,
-                  listTextStyle: GoogleFonts.josefinSans(color: Theme.of(context).colorScheme.inversePrimary.withOpacity(0.9)),
-                  textStyle: GoogleFonts.josefinSans(color: Theme.of(context).colorScheme.inversePrimary),
-                  searchTextStyle: GoogleFonts.josefinSans(color: Theme.of(context).colorScheme.inversePrimary.withOpacity(0.65)),
-                  searchDecoration: InputDecoration(
-                    enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Theme.of(context).colorScheme.inversePrimary.withOpacity(0.65))),
-                    focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Theme.of(context).colorScheme.onPrimary)),
-                  ),
-                  textFieldDecoration: InputDecoration(
-                    label: Text('Select Category', style: GoogleFonts.josefinSans(color: Theme.of(context).colorScheme.onPrimary, fontSize: 16)),
-                    filled: true,
-                    fillColor: Theme.of(context).colorScheme.primary,
-                    enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).colorScheme.inversePrimary.withOpacity(0.65)), borderRadius: BorderRadius.circular(10)),
-                    focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).colorScheme.onPrimary), borderRadius: BorderRadius.circular(10)),
-                  ),
-                  dropDownIconProperty: IconProperty(color: Theme.of(context).colorScheme.onPrimary),
-                  clearIconProperty: IconProperty(color: Theme.of(context).colorScheme.onPrimary),
-                  onChanged: (value) {
-                    categoryController.text = categoryDropdownController.dropDownValue!.name;
-                  },
-                ),
-              ),
-              const SizedBox(width: 10),
-              Flexible(
-                flex: 1,
-                child: TextField(
-                  controller: categoryController,
-                  keyboardType: TextInputType.text,
-                  style: GoogleFonts.josefinSans(color: Theme.of(context).colorScheme.inversePrimary, fontSize: 16),
-                  cursorColor: Theme.of(context).colorScheme.onPrimary,
-                  decoration: InputDecoration(
-                    label: Text('New Category', style: GoogleFonts.josefinSans(color: Theme.of(context).colorScheme.onPrimary, fontSize: 16)),
-                    filled: true,
-                    fillColor: Theme.of(context).colorScheme.primary,
-                    enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).colorScheme.inversePrimary.withOpacity(0.65)), borderRadius: BorderRadius.circular(10)),
-                    focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).colorScheme.onPrimary), borderRadius: BorderRadius.circular(10)),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              children: [
+                Flexible(
+                  flex: 1,
+                  child: DropDownTextField(
+                    dropDownList: List.generate(categoryList.length, (index) => DropDownValueModel(name: categoryList[index], value: index)),
+                    controller: categoryDropdownController,
+                    enableSearch: true,
+                    keyboardType: TextInputType.text,
+                    listTextStyle: GoogleFonts.josefinSans(color: Theme.of(context).colorScheme.inversePrimary.withOpacity(0.9)),
+                    textStyle: GoogleFonts.josefinSans(color: Theme.of(context).colorScheme.inversePrimary),
+                    searchTextStyle: GoogleFonts.josefinSans(color: Theme.of(context).colorScheme.inversePrimary.withOpacity(0.65)),
+                    searchDecoration: InputDecoration(
+                      enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Theme.of(context).colorScheme.inversePrimary.withOpacity(0.65))),
+                      focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Theme.of(context).colorScheme.onPrimary)),
+                    ),
+                    textFieldDecoration: InputDecoration(
+                      label: Text('Select Category', style: GoogleFonts.josefinSans(color: Theme.of(context).colorScheme.onPrimary, fontSize: 16)),
+                      filled: true,
+                      fillColor: Theme.of(context).colorScheme.primary,
+                      enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).colorScheme.inversePrimary.withOpacity(0.65)), borderRadius: BorderRadius.circular(10)),
+                      focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).colorScheme.onPrimary), borderRadius: BorderRadius.circular(10)),
+                    ),
+                    dropDownIconProperty: IconProperty(color: Theme.of(context).colorScheme.onPrimary),
+                    clearIconProperty: IconProperty(color: Theme.of(context).colorScheme.onPrimary),
+                    onChanged: (value) {
+                      categoryController.text = categoryDropdownController.dropDownValue!.name;
+                    },
                   ),
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 10),
-          GestureDetector(
-            onTap: () => _selectDate(context),
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Theme.of(context).colorScheme.onPrimary),
-                borderRadius: BorderRadius.circular(12),
-                color: Theme.of(context).colorScheme.primary,
-              ),
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 14),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(DateFormat('dd.MM.yyyy').format(_selectedDate), style: GoogleFonts.josefinSans(color: Theme.of(context).colorScheme.onPrimary, fontSize: 18)),
-                  const SizedBox(width: 8),
-                  Icon(Icons.calendar_month_rounded, color: Theme.of(context).colorScheme.onPrimary, size: 22),
-                ],
+                const SizedBox(width: 10),
+                Flexible(
+                  flex: 1,
+                  child: TextField(
+                    controller: categoryController,
+                    keyboardType: TextInputType.text,
+                    style: GoogleFonts.josefinSans(color: Theme.of(context).colorScheme.inversePrimary, fontSize: 16),
+                    cursorColor: Theme.of(context).colorScheme.onPrimary,
+                    decoration: InputDecoration(
+                      label: Text('New Category', style: GoogleFonts.josefinSans(color: Theme.of(context).colorScheme.onPrimary, fontSize: 16)),
+                      filled: true,
+                      fillColor: Theme.of(context).colorScheme.primary,
+                      enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).colorScheme.inversePrimary.withOpacity(0.65)), borderRadius: BorderRadius.circular(10)),
+                      focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Theme.of(context).colorScheme.onPrimary), borderRadius: BorderRadius.circular(10)),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            GestureDetector(
+              onTap: () => _selectDate(context),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Theme.of(context).colorScheme.onPrimary),
+                  borderRadius: BorderRadius.circular(12),
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 14),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(DateFormat('dd.MM.yyyy').format(_selectedDate), style: GoogleFonts.josefinSans(color: Theme.of(context).colorScheme.onPrimary, fontSize: 18)),
+                    const SizedBox(width: 8),
+                    Icon(Icons.calendar_month_rounded, color: Theme.of(context).colorScheme.onPrimary, size: 22),
+                  ],
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 16),
-          GestureDetector(
-            onTap: _addExpence,
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Theme.of(context).colorScheme.onSecondary),
-                borderRadius: BorderRadius.circular(12),
-                color: Theme.of(context).colorScheme.primary,
+            const SizedBox(height: 16),
+            GestureDetector(
+              onTap: _addExpence,
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Theme.of(context).colorScheme.onSecondary),
+                  borderRadius: BorderRadius.circular(12),
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                padding: const EdgeInsets.all(8),
+                child: Text('Confirm', style: GoogleFonts.josefinSans(color: Theme.of(context).colorScheme.onSecondary, fontSize: 20)),
               ),
-              padding: const EdgeInsets.all(8),
-              child: Text('Confirm', style: GoogleFonts.josefinSans(color: Theme.of(context).colorScheme.onSecondary, fontSize: 20)),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/components/floating_button.dart
+++ lib/components/floating_button.dart
@@ -9,17 +9,19 @@ class FloatingButton extends StatelessWidget {
     void showAddExpenceModal() {
       showModalBottomSheet(
         context: context,
-        // isScrollControlled: true,
+        isScrollControlled: true,
+        // useSafeArea: true,
         shape: const RoundedRectangleBorder(borderRadius: BorderRadius.vertical(top: Radius.circular(16.0))),
         clipBehavior: Clip.antiAliasWithSaveLayer,
         builder: (context) {
-          return Scaffold(
-            extendBody: false,
-            // key: modelScaffoldKey,
-            resizeToAvoidBottomInset: true,
-            backgroundColor: Theme.of(context).colorScheme.secondary,
-            body: const AddExpenceModal(),
-          );
+          //   return Scaffold(
+          //     extendBody: false,
+          //     // key: modelScaffoldKey,
+          //     // resizeToAvoidBottomInset: true,
+          //     backgroundColor: Theme.of(context).colorScheme.secondary,
+          //     body: const AddExpenceModal(),
+          //   );
+          return const AddExpenceModal();
         },
       );
     }
